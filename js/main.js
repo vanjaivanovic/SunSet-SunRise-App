ea8f73b8ab9a424efe15c0f4979b4f88fe67c253
Vanja Ivanovic <vanja.i@hotmail.com>
2018-03-15 11:06:23 +0100

    Geolocation från google APIet lagras nu i Sunset/sunrise APIet. Hämtar data med longitude och latitude frånvald plats i inputfältet

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var googleKey = "AIzaSyBw0MIypLnDtsAuC9ZIBD0TnMuhhZ_53OM";
+
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -11,35 +11,35 @@ function initialize() {
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var place = autocomplete.getPlace();
         var location = "<b>Adress</b>: " + place.formatted_address + "<br/>";
-        var latitude = "<b>Latitude</b>" + place.geometry.location.lat(); + "<br/>";
-        var longitude = "<b>Longitude</b>" + place.geometry.location.lng();
-        var lat = place.geometry.location.lat();
-        var long = place.geometry.location.lng();
+        var lat = "<b>Latitude</b>" + place.geometry.location.lat(); + "<br/>";
+        var long = "<b>Longitude</b>" + place.geometry.location.lng();
+        var latitude = place.geometry.location.lat();
+        var longitude = place.geometry.location.lng();
  
         document.getElementById('adressResult').innerHTML = location;
+        document.getElementById('latitudeResult').innerHTML = lat
+        document.getElementById('longitudeResult').innerHTML = long;
 
-        document.getElementById('latitudeResult').innerHTML = latitude;
-
-        document.getElementById('longitudeResult').innerHTML = longitude;
-
- 
-
-        console.log(location);
-
-        console.log(latitude);
-
-        console.log(longitude);
-
-        console.log(UTCoffset);
-
-        getsunlocation(lat, long);
+        getsunlocation(latitude, longitude);
 
  
     });
-
- 
-
 };
+
+function getsunlocation(latitude, longitude){
+  fetch(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`)
+    .then((response) => response.json())
+    .then((sunData) =>  {
+      console.log(sunData)
+      displaySunData(sunData);
+
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
+
 var button = document.getElementById("CurrentPosition");
 var errormessage = document.getElementById("positionError");
 
